1eb34e934ed15c00fd4a210662bbc7f5dff63b26
Fred Trotter <fred.trotter@gmail.com>
2013-07-17 07:20:49 +0000

    recompiled based on data changes

---
+++ app/models/Nonphysician.php
@@ -0,0 +1,9 @@
+<?php
+	
+
+	class Nonphysician extends  NonphysicianBase{ 
+		//put custom code here... look in the base class for generated relations..
+		
+	}
+
+?>
---
+++ app/models/ProviderType.php
@@ -0,0 +1,9 @@
+<?php
+	
+
+	class ProviderType extends  ProviderTypeBase{ 
+		//put custom code here... look in the base class for generated relations..
+		
+	}
+
+?>
